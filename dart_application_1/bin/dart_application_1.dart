9fef12370a9411d5d6ff4a188150d835edad6782
RoopamTaneja <roopamtaneja999@gmail.com>
2023-07-10 18:19:33 +0530

    console template

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,15 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1.calculate()}!');
+}
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter username : ");
+//   final input = stdin.readLineSync();
+//   print(input);
+// }
+
---
+++ signup_register.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter username : ");
+  final input = stdin.readLineSync();
+  print(input);
+}
