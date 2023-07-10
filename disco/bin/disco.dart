f1e8b89fd8a3fc66117b67eb102c0c0af2760661
DRDiv <divij.rawal@gmail.com>
2023-07-11 02:37:58 +0530

    User registration attempted

---
+++ disco/bin/disco.dart
@@ -0,0 +1,9 @@
+#!/usr/bin/env dart
+
+import 'package:disco/create_user.dart' as create_user;
+
+void main(List<String> arguments) {
+  if (arguments[0] == "register") {
+    create_user.main(arguments);
+  }
+}
